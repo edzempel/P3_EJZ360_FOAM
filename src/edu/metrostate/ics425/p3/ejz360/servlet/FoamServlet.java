739cd895f33dfcc496485190461617d456f96148
edzempel <edward.zempel@gmail.com>
2019-10-02 22:22:40 -0500

    add FoamServlet

---
+++ src/edu/metrostate/ics425/p3/ejz360/servlet/FoamServlet.java
@@ -0,0 +1,45 @@
+package edu.metrostate.ics425.p3.ejz360.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FoamServlet
+ * @author ezempel
+ */
+@WebServlet({ "/FoamServlet", "/view", "/add", "/edit", "/delete" })
+public class FoamServlet extends HttpServlet {
+	private static final long serialVersionUID = 20191002L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FoamServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
