c8544b62dd5291d44b3b70e0dbe037b1f88c60a3
edzempel <edward.zempel@gmail.com>
2019-10-06 14:12:18 -0500

    update FoamServlet.java
    Added action origin conditions

--- src/edu/metrostate/ics425/p3/ejz360/servlet/FoamServlet.java
+++ src/edu/metrostate/ics425/p3/ejz360/servlet/FoamServlet.java
@@ -1,20 +1,26 @@
 package edu.metrostate.ics425.p3.ejz360.servlet;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edu.metrostate.ics425.p3.ejz360.model.AthleteBean;
+
 /**
  * Servlet implementation class FoamServlet
  * 
  * @author ezempel
  */
-@WebServlet({ "/FoamServlet", "/view", "/add", "/edit", "/delete" })
+@WebServlet({ "/FoamServlet", "/view", "/add", "/edit", "/delete"})
 public class FoamServlet extends HttpServlet {
 	private static final long serialVersionUID = 20191002L;
+	private ArrayList<AthleteBean> roster = null;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -38,19 +44,46 @@ public class FoamServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+
 		processRequest(request, response);
 	}
 
 	private void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
-		if (true) {
+		roster = new ArrayList<AthleteBean>();
+
+		var sc = getServletContext();
+
+		// get current action
+		String action = request.getParameter("action");
+		if (action == null) {
+			action = "view";
+		}
+
+		if (action == "view") {
 			String welcome = "Welcome to the Freedonia Olympic" + "Athlete Management System (FOAMS).";
 			request.setAttribute("welcome", welcome);
+
+		} else if (action == "add") {
+			AthleteBean newAthlete = createAthlete(request.getParameter("newID"), request.getParameter("newFirst"),
+					request.getParameter("newDob"), LocalDate.parse(request.getParameter("newLast")));
+			roster.add(newAthlete);
 		} else {
 			request.setAttribute("errMsg", "Invalid input");
 		}
+
+		request.setAttribute("roster", roster);
 		request.getRequestDispatcher("index.jsp").forward(request, response);
+	}
+
+	private AthleteBean createAthlete(String newID, String newFirst, String newLast, LocalDate newDob) {
+		AthleteBean ab = new AthleteBean();
+		ab.setFirstName(newFirst);
+		ab.setLastName(newLast);
+		ab.setNationalID(newID);
+		ab.setDateOfBirth(newDob);
+		return ab;
 	}
 
 }
